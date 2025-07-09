7551996f5fe9cc61012f739eadc24ceea60c1382
German220203 <gerojegar@alum.us.es>
2025-07-09 14:26:28 +0200

    first commit

---
+++ repairweb/src/main/java/es/relogic/repairweb/RepairwebApplication.java
@@ -0,0 +1,13 @@
+package es.relogic.repairweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairwebApplication.class, args);
+	}
+
+}
---
+++ repairweb/src/test/java/es/relogic/repairweb/RepairwebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.relogic.repairweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
