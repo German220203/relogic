0b9825b2c83239c9fb594a6cacbe8c127ffec3cb
German220203 <gerojegar@alum.us.es>
2025-07-09 15:21:23 +0200

    first commit

---
+++ src/main/java/es/relogic/relogic/RelogicApplication.java
@@ -0,0 +1,13 @@
+package es.relogic.relogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/relogic/relogic/RelogicApplicationTests.java
@@ -0,0 +1,13 @@
+package es.relogic.relogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
