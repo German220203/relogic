80c37699eca42d9ad2baf8294ebaf5602a394866
German220203 <gerojegar@alum.us.es>
2025-10-16 16:55:34 +0200

    CORS en WebConfig

--- src/main/java/es/relogic/relogic/configuration/WebConfig.java
+++ src/main/java/es/relogic/relogic/configuration/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://relogic.com, http://frontend:3000")
+                        .allowedOrigins("http://localhost:3000", "https://relogic.com", "http://frontend:3000", "https://relogic.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
