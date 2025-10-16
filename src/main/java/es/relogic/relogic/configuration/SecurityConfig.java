89c00f3ec5bbf8ffa1c5a86f700a131c085c905f
German220203 <gerojegar@alum.us.es>
2025-10-16 17:00:27 +0200

    añadido dominio frontend para evitar CORS

--- src/main/java/es/relogic/relogic/configuration/SecurityConfig.java
+++ src/main/java/es/relogic/relogic/configuration/SecurityConfig.java
@@ -58,9 +58,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "https://relogic.onrender.com"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         configuration.setAllowCredentials(true); // muy importante si estás enviando cookies o headers auth
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
